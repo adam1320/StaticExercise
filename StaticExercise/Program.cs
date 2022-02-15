70cfbbff9a1cc57036afa12c8e81a2a8e452b7e3
Adam Brannon <adam1320@rocketmail.com>
2022-02-15 09:39:43 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+
+            var CTemp = TempConverter.FahrenheitToCelsius(68);
+            Console.WriteLine($"68 F converts to {CTemp} C.");
+
+            var FTemp = TempConverter.CelsiusToFahrenheit(20);
+            Console.WriteLine($"20 C converts to {FTemp} F.");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double temp)
+        {
+            double FToC = (temp -  32) / 1.8;
+            return FToC;
+        }
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            double CToF = (temp * 9) / 5 + 32;
+            return CToF;
+        }
+    }
+}
